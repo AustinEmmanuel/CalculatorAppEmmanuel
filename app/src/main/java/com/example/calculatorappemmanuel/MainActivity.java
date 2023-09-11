0d36075d82d3385e978e103dba28507d39b03492
AustinEmmanuel <aemmanuel0263@gmail.com>
2023-09-11 08:18:34 -0500

    9/11__Started to code. Got my add, subtract, multiply and divide methods to work

--- app/src/main/java/com/example/calculatorappemmanuel/MainActivity.java
+++ app/src/main/java/com/example/calculatorappemmanuel/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.calculatorappemmanuel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void add(View v){
+        EditText num1 = findViewById(R.id.num_One);
+        double num1D = Double.parseDouble(num1.getText().toString());
+        EditText num2 = findViewById(R.id.num_Two);
+        double num2D = Double.parseDouble(num2.getText().toString());
+
+        EditText answer = findViewById(R.id.num_answer);
+        String answerValue = String.valueOf((num1D+num2D));
+        answer.setText(answerValue);
+    }
+
+    public void subtract(View v){
+        EditText num1 = findViewById(R.id.num_One);
+        double num1D = Double.parseDouble(num1.getText().toString());
+        EditText num2 = findViewById(R.id.num_Two);
+        double num2D = Double.parseDouble(num2.getText().toString());
+
+        EditText answer = findViewById(R.id.num_answer);
+        String answerValue = String.valueOf((num1D-num2D));
+        answer.setText(answerValue);
+    }
+
+    public void multiply(View v){
+        EditText num1 = findViewById(R.id.num_One);
+        double num1D = Double.parseDouble(num1.getText().toString());
+        EditText num2 = findViewById(R.id.num_Two);
+        double num2D = Double.parseDouble(num2.getText().toString());
+
+        EditText answer = findViewById(R.id.num_answer);
+        String answerValue = String.valueOf((num1D*num2D));
+        answer.setText(answerValue);
+    }
+
+    public void division(View v){
+        EditText num1 = findViewById(R.id.num_One);
+        double num1D = Double.parseDouble(num1.getText().toString());
+        EditText num2 = findViewById(R.id.num_Two);
+        double num2D = Double.parseDouble(num2.getText().toString());
+
+        EditText answer = findViewById(R.id.num_answer);
+        String answerValue = String.valueOf((num1D/num2D));
+        answer.setText(answerValue);
     }
 }
